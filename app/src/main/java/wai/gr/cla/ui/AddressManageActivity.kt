5ca7fa61060eec9f807ff422a397c1686302a721
Finderchangchang <1031066280@qq.com>
2017-10-18 23:26:08 +0800

    123

--- app/src/main/java/wai/gr/cla/ui/AddressManageActivity.kt
+++ app/src/main/java/wai/gr/cla/ui/AddressManageActivity.kt
@@ -1,10 +1,17 @@
 package wai.gr.cla.ui
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 
 import wai.gr.cla.R
 import wai.gr.cla.base.BaseActivity
+import com.lljjcoder.citylist.CityListSelectActivity
+import android.support.v4.app.ActivityCompat.startActivityForResult
+import com.lljjcoder.citylist.bean.CityInfoBean
+
 
 class AddressManageActivity : BaseActivity() {
     override fun setLayout(): Int {
